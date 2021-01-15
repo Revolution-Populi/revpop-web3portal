9790274bcc5954235b038ef58c611c7bc5da88f9
walteratwork <72606171+walteratwork@users.noreply.github.com>
2021-01-15 12:15:18 +0300

    Added favicon

--- app/assets/loader.js
+++ app/assets/loader.js
@@ -1,5 +1,5 @@
 require("./stylesheets/app.scss");
-require("file-loader?name=favicon.png!./favicon.png");
+require("file-loader?name=favicon.ico!./favicon.ico");
 require("babel-polyfill");
 require("whatwg-fetch");
 require("indexeddbshim");

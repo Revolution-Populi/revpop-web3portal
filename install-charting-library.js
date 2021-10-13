38835cdf3a352c9a0c1ca7d2b0ecde26a8924147
walteratwork <72606171+walteratwork@users.noreply.github.com>
2021-10-13 16:57:28 +0300

    Fixed charting library build problem on Node > 12

--- install-charting-library.js
+++ install-charting-library.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var path = require("path");
 const md5File = require("md5-file");
 const http = require("https");
+const {exec} = require("child_process");
 var extract = require("extract-zip");
 
 function getMD5Digest(file) {
@@ -9,30 +10,39 @@ function getMD5Digest(file) {
     return hash.toUpperCase();
 }
 
+const dir = path.join(__dirname, "charting_library");
 var outputFileName = "charting_library.17.025.02b61a1c.zip";
-var outputFilePath = path.join(__dirname, outputFileName);
+var outputFilePath = path.join(dir, outputFileName);
 const outputFile = fs.createWriteStream(outputFilePath);
 
-http.get("https://bitshares.org/assets/" + outputFileName, (response) => {
+http.get("https://bitshares.org/assets/" + outputFileName, response => {
     response.pipe(outputFile);
-}).on("error", (err) => {
+}).on("error", err => {
     console.error("Failed to download charting_library archive");
     console.error(err);
-    throw (err);
+    throw err;
 });
 
 outputFile.on("finish", () => {
     const actualDigest = getMD5Digest(outputFilePath);
-    const expectedDigest = fs.readFileSync(outputFilePath + ".md5").toString().trim();
+    const expectedDigest = fs
+        .readFileSync(outputFilePath + ".md5")
+        .toString()
+        .trim();
     if (actualDigest !== expectedDigest) {
         fs.unlinkSync(outputFilePath);
-        throw new Error("MD5 of downloaded file (" + actualDigest + ") not matches expected (" + expectedDigest + ")");
+        throw new Error(
+            "MD5 of downloaded file (" +
+                actualDigest +
+                ") not matches expected (" +
+                expectedDigest +
+                ")"
+        );
     }
     console.log("MD5 digest validated, extracting library...");
-    extract(outputFilePath, {dir: __dirname}, function (err) {
+    extract(outputFilePath, {dir}, function(err) {
         if (err) {
             console.error("Decompress error!", err);
         }
     });
 });
-
--- package-charting-library.js
+++ package-charting-library.js
@@ -3,10 +3,21 @@ var path = require("path");
 var archiver = require("archiver");
 const md5File = require("md5-file");
 
-
-var outputFile = path.join(__dirname, "charting_library.17.025.02b61a1c.zip");
+const dir = path.join(__dirname, "charting_library");
+var outputFile = path.join(dir, "charting_library.17.025.02b61a1c.zip");
 const outFilename = path.basename(outputFile);
-var ignoreFiles = [path.basename(__filename), outFilename, outFilename + ".md5", "install-charting-library.js", "scss", "scss/**", "base.css", "darkTheme.css", "midnightTheme.css", "lightTheme.css"];
+var ignoreFiles = [
+    dir,
+    outFilename,
+    outFilename + ".md5",
+    "install-charting-library.js",
+    "scss",
+    "scss/**",
+    "base.css",
+    "darkTheme.css",
+    "midnightTheme.css",
+    "lightTheme.css"
+];
 
 /**
  * @param {String} source
@@ -14,14 +25,14 @@ var ignoreFiles = [path.basename(__filename), outFilename, outFilename + ".md5",
  * @returns {Promise}
  */
 function zipDirectory(source, out) {
-    const archive = archiver("zip", { zlib: { level: 9 }});
+    const archive = archiver("zip", {zlib: {level: 9}});
     const stream = fs.createWriteStream(out);
 
     return new Promise((resolve, reject) => {
         archive
             .glob("**", {
                 cwd: source,
-                ignore: ignoreFiles,
+                ignore: ignoreFiles
             })
             .on("error", err => reject(err))
             .pipe(stream);
@@ -35,11 +46,10 @@ function getMD5Digest(file) {
     return hash.toUpperCase();
 }
 
-zipDirectory(__dirname, outputFile)
-    .then(() => {
-        console.log("Input folder compressed to " + outputFile);
-        var digest = getMD5Digest(outputFile);
-        console.log("MD5: " + digest);
-        fs.writeFileSync(outputFile + ".md5", digest);
-        process.exit(0);
-    });
+zipDirectory(dir, outputFile).then(() => {
+    console.log("Input folder compressed to " + outputFile);
+    var digest = getMD5Digest(outputFile);
+    console.log("MD5: " + digest);
+    fs.writeFileSync(outputFile + ".md5", digest);
+    process.exit(0);
+});

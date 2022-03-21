5a68a7d4935a22a78bd8e9657b6f435b498b1e5b
trafterian <99242625+Trafterian@users.noreply.github.com>
2022-03-21 13:26:01 +0300

    feature/tests
    Add enzyme

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function Hello(props: {name?: string}) {
+    let title = "Hey, stranger!";
+
     if (props.name) {
-        return <h1>Hello, {props.name}!</h1>;
-    } else {
-        return <h1>Hey, stranger</h1>;
+        title = `Hello, ${props.name}!`;
     }
+
+    return <Header title={title} />;
+}
+
+export function Header(props: {title: string}) {
+    return <h1>{props.title}</h1>;
 }

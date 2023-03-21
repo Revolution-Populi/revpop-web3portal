9a89a569e597e953538e064615d728c8d4532bda
Griffics <126185286+Griffics@users.noreply.github.com>
2023-03-21 20:37:03 +0200

    Update preimage encoding

--- app/components/Modal/HtlcModal.jsx
+++ app/components/Modal/HtlcModal.jsx
@@ -127,7 +127,6 @@ class Preimage extends React.Component {
                     preimage_cipher: preimage_cipher,
                     preimage_size: preimage.length
                 });
-
                 this.setState({
                     preimage_hash_calculated: hash
                 });

f6d8746aa77b87bf639bed91fa4c594377104874
Griffics <126185286+Griffics@users.noreply.github.com>
2023-03-29 20:46:18 +0300

    Show Explorer link to the correct network

--- app/Context/Deposit/Application/Command/MakeDeposit/MakeDepositHandler.ts
+++ app/Context/Deposit/Application/Command/MakeDeposit/MakeDepositHandler.ts
@@ -44,8 +44,6 @@ export default class MakeDepositHandler {
             throw new BlockchainConnectionError();
         }
 
-        console.log("CONTRACT ", createNewExternalContractRequest);
-
         const externalContract = ExternalContract.create(
             createContractResponse.txHash
         );
